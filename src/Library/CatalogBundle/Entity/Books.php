ebd2a2584ca91aaeeab6c8d7c61f81c277ba6329
Stanislav Tibilius <tibilius@gmail.com>
2015-05-19 18:27:45 +0300

    fix err

--- src/Library/CatalogBundle/Entity/Books.php
+++ src/Library/CatalogBundle/Entity/Books.php
@@ -466,7 +466,7 @@ class Books
         if ($image) {
             // It is required that at least one field changes if you are using doctrine
             // otherwise the event listeners won't be called and the file is lost
-            $this->updated = time();
+            $this->setUpdated(new \DateTime("now"));
         }
     }
 
