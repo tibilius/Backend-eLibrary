03857e2a92bcec62d949b950427f3ceef3348f2d
Stanislav Tibilius <tibilius@gmail.com>
2015-06-10 14:49:06 +0300

    fix

--- app/DoctrineMigrations/Version20150609150149.php
+++ app/DoctrineMigrations/Version20150609150149.php
@@ -16,19 +16,19 @@ class Version20150609150149 extends AbstractMigration
     public function up(Schema $schema)
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE books DROP CONSTRAINT fk_4a1b2a92a32efc6');
-        $this->addSql('ALTER TABLE books
-          ADD CONSTRAINT fk_4a1b2a92a32efc6 FOREIGN KEY (rating_id)
-              REFERENCES rating (id) MATCH SIMPLE
-              ON UPDATE NO ACTION ON DELETE SET NULL
-        ');
-
-        $this->addSql('ALTER TABLE books DROP CONSTRAINT fk_4a1b2a92e2904019');
-        $this->addSql('ALTER TABLE books
-          ADD CONSTRAINT fk_4a1b2a92e2904019 FOREIGN KEY (thread_id)
-              REFERENCES thread (id) MATCH SIMPLE
-              ON UPDATE NO ACTION ON DELETE SET NULL
-      ');
+//        $this->addSql('ALTER TABLE books DROP CONSTRAINT fk_4a1b2a92a32efc6');
+//        $this->addSql('ALTER TABLE books
+//          ADD CONSTRAINT fk_4a1b2a92a32efc6 FOREIGN KEY (rating_id)
+//              REFERENCES rating (id) MATCH SIMPLE
+//              ON UPDATE NO ACTION ON DELETE SET NULL
+//        ');
+//
+//        $this->addSql('ALTER TABLE books DROP CONSTRAINT fk_4a1b2a92e2904019');
+//        $this->addSql('ALTER TABLE books
+//          ADD CONSTRAINT fk_4a1b2a92e2904019 FOREIGN KEY (thread_id)
+//              REFERENCES thread (id) MATCH SIMPLE
+//              ON UPDATE NO ACTION ON DELETE SET NULL
+//      ');
 
 
     $this->addSql('ALTER TABLE readlists_books  ADD COLUMN "position" integer');
